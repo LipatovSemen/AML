0fa7e55c8960a12a7fd0f069625a8ba2d7f5fb89
LipatovSemen <149626956+LipatovSemen@users.noreply.github.com>
2023-11-06 17:25:48 +0300

    sequence.sql

---
+++ sequence.sql
@@ -0,0 +1,18 @@
+-- Start of DDL Script for Sequence GC.AML_SEQ
+-- Generated 06-ноя-2023 17:25:19 from GC@BANK
+
+CREATE SEQUENCE aml_seq
+  INCREMENT BY 1
+  START WITH 79099
+  MINVALUE 1
+  MAXVALUE 9999999999999999999999999999
+  NOCYCLE
+  NOORDER
+  CACHE 20
+  NOKEEP
+  GLOBAL
+/
+
+
+-- End of DDL Script for Sequence GC.AML_SEQ
+
